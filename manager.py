21e470f3a2e59e199ef90185cd810253754dd760
vabs <njoy.today@gmail.com>
2016-12-23 03:50:08 -0500

    fixing migrations issue with proper code in __init__.py in models module

--- manager.py
+++ manager.py
@@ -3,19 +3,14 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 
-from appname import app
-
-db = SQLAlchemy(app)
+from appname import app, db
 
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-# from appname.models import *
-
-from appname.models.points import Points
-from appname.models.test import test
+from appname.models import *
 
 if __name__ == '__main__':
     manager.run()
